2b45cf7d0718fbe8f2ca1bcc8049f84d5cab851e
Banti Prajapati <banti.prajapati@systematixindia.com>
2021-08-30 18:41:31 +0530

    start the new project of router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import { Route, Switch, Redirect } from "react-router-dom";
-import Welcome from "./pages/welcome";
-import Products from "./pages/products";
-import MainHeader from "./Header/MainHeader";
-import ProductDetails from "./pages/productDetails";
-
 function App() {
   return (
-    <>
-      <header>
-        <MainHeader />
-      </header>
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            {/* here is colon ":" mandotory bcz we are add other path */}
-            <ProductDetails />
-          </Route>
-        </Switch>
-      </main>
-    </>
+    <div>
+
+    </div>
   );
 }
 
